60c613b69851c05db605ca409ce77864275b6089
thealoksingh <121547815+thealoksingh@users.noreply.github.com>
2023-07-29 15:07:28 +0530

    Add files via upload

---
+++ Downloads.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello world")
+
+
+# In[2]:
+
+
+Print("hello world") #python is case-sensitive
+
+
+# In[3]:
+
+
+print(hello world) #it should be in inverted comma for string
+
+
+# In[4]:
+
+
+print(10)
+
+
+# In[5]:
+
+
+print(10.7) 
+
+
+# ### ;agvvshvhvsvhsgh
+
+# In[6]:
+
+
+###;agvvshvhvsvhsgh #keep the cursor on in[] and press m and run that
+
+
+# #### sgssvghvshh
+
+# ##### printhello
+
+# ##sbhsb
+
+# ## shbshh
+
+# ##shhsjhj
+
+# In[10]:
+
+
+#sjsbj
+
+
+# # jhsjsjs
+
+# #bjsbjbsj
+
+# 
+
+# ##### ahhsj
+
+# In[11]:
+
+
+### 1.variable name should not have space
+First name= "alok singh"
+
+
+# In[13]:
+
+
+### 2.variable name should start with A-Z ,a-z or _
+First name= "alok singh"
+
+
+# In[14]:
+
+
+First name= "alok singh"
+
+
+# In[15]:
+
+
+First name="alok singh"
+
+
+# In[16]:
+
+
+First name="aloksingh"
+
+
+# In[17]:
+
+
+Firstname="alok singh"
+name="alok"
+_name="alok"
+
+
+# In[18]:
+
+
+Firstname="alok singh"
+name="alok"
+_name="alok"
+
+
+# In[19]:
+
+
+#variable name shoud not start with number but number cam be present anywhere
+
+
+# In[20]:
+
+
+#and only allowed speacial charater is _ in variable
+
+
+# ## Data types
+
+# In[21]:
+
+
+x=10.5
+type(x)
+
+
+# In[22]:
+
+
+y=10
+type(y)
+
+
+# In[23]:
+
+
+z=True #use capital T 
+type(z)
+
+
+# In[24]:
+
+
+x=10 #it will be executing only last line
+type(x)
+x
+
+
+# In[25]:
+
+
+x,type(x)
+
+
+# In[26]:
+
+
+x,y=10,20
+x
+y
+
+
+# In[27]:
+
+
+x,y
+
+
+# In[28]:
+
+
+y,x
+
+
+# ### Data type conversion
+
+# #### (1) implicit-> python interpretor without programmer's intervention
+# #### (2) explict ->user-defined ->casting
+
+# In[29]:
+
+
+#implicit conversion
+# int and float
+x=1.5
+y=3
+z=x*y
+type(z)
+
+
+# In[30]:
+
+
+x="data"
+y=1.5
+z=x*y
+type(z)
+
+
+# In[31]:
+
+
+x=20
+y=10
+z=x/y
+type(z)
+
+
+# In[32]:
+
+
+z
+
+
+# In[33]:
+
+
+#explicit conversion
+x="2"
+y=1.5
+z=x+y
+type(z)
+
+
+# In[34]:
+
+
+z=int(x)+y
+z
+
+
+# ### type casting
+
+# #### int(),float(),bool()
+
+# #### ,str()
+
+# In[35]:
+
+
+int-> float ->possible
+int->bool ->possible
+int ->string->possible
+
+
+# In[36]:
+
+
+x=5
+str(x)
+
+
+# In[37]:
+
+
+x=0
+bool(x)
+
+
+# In[ ]:
+
+
+
+
